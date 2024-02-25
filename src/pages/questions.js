c764e4581ce1a4de74c3cecf5af49d57ab4f0614
chickenjenders <tylerjenny8@gmail.com>
2024-02-25 17:47:19 -0500

    ups

---
+++ src/pages/questions.js
@@ -0,0 +1,103 @@
+export const qnaList = [
+  {
+    question: "When picturing your vacation, the landscape is mostly...",
+    answers: [
+      "Bustling city streets with iconic landmarks",
+      "Rolling hills, vineyards, and charming villages",
+      "Pristine beaches and turquoise waters",
+      "Snow-capped mountains and cozy, rustic towns",
+      "Lush rainforests and exotic wildlife encounters",
+    ],
+  },
+  {
+    question: "Your ideal climate looks like...?",
+    answers: [
+      "Warm sunshine and endless summer days",
+      "A pleasant mix of seasons with mild temperatures",
+      "Fresh mountain air and potential for snowy fun",
+      "Tropical heat and humidity for that exotic feel",
+      "Anything unpredictable - I'm up for any kind of weather adventure",
+    ],
+  },
+  {
+    question: "What's your preferred accommodation style?",
+    answers: [
+      "Luxury hotels",
+      "Unique stays",
+      "Budget-friendly and simple",
+      "Camping or outdoors",
+      "Anything close to sight-seeing areas",
+    ],
+  },
+  {
+    question: "How would you describe your ideal vacation pace?",
+    answers: [
+      "Nonstop adventure - every day is packed!",
+      "Mix of exploring and relaxing",
+      "Mostly chill time with a few activities",
+      "Total relaxation, please!",
+      "Whatever fits the destination",
+    ],
+  },
+  {
+    question: "The perfect vacation day includes...",
+    answers: [
+      "Exploring museums, art galleries, and historical sites",
+      "Wandering local markets, sampling food, and people-watching",
+      "Thrilling outdoor adventures like hiking, biking, or watersports",
+      "Sunbathing, swimming, and enjoying laid-back beach vibes",
+      "Seeking unique off-the-beaten-path experiences",
+    ],
+  },
+  {
+    question: "My idea of nightlife is...",
+    answers: [
+      "Vibrant clubs, rooftop bars, and live music venues",
+      "Cozy pubs, live theater, and intimate cultural performances",
+      "Stargazing, campfires, and bonding with fellow travelers",
+      "Beachfront bars, sunset cruises, and relaxing evenings",
+      "I prefer early nights to wake up refreshed",
+    ],
+  },
+  {
+    question: "Your favorite flavors tend to be...",
+    answers: [
+      "Bold, spicy, and packed with exotic ingredients",
+      "Fresh, light, and inspired by the sea",
+      "Hearty, comforting, and rich with tradition",
+      "Sweet, fruity, and refreshing",
+      "Unusual and unexpected – I'm ready to try anything!",
+    ],
+  },
+  {
+    question: "Which of these activities would you never want to miss?",
+    answers: [
+      "Visiting a world-famous landmark or UNESCO site",
+      "Seeing an iconic music, theater, or sporting event",
+      "Experiencing a local festival or celebration",
+      "Trying a hands-on activity like cooking or crafting",
+      "Learning about the destination's history and culture in depth",
+    ],
+  },
+  {
+    question: "Your dream travel companion is...",
+    answers: [
+      "Going solo for a personal adventure",
+      "My significant other for a romantic escape",
+      "A group of close friends for endless fun",
+      "My family, making memories together",
+      "Joining a tour group to meet new people",
+    ],
+  },
+  {
+    question:
+      "If you could have one special experience on this trip, what would it be?",
+    answers: [
+      "Witnessing a natural wonder like the Northern Lights",
+      "Sampling my way through a world-famous culinary scene",
+      "Learning a new skill, like surfing or a foreign language",
+      "Staying in a unique place – a castle, treehouse, etc",
+      "I'm open to surprises!",
+    ],
+  },
+];
